e25baafbb7ad16d33d01141fe414ce7d92dd1634
lukasze <wyspianski.lukasz@gmail.com>
2022-06-11 10:41:01 +0200

    refactor do whenGetBooks_thenReturnHTTP200, gotowy test (red)

--- src/test/java/com/example/demotest/book/BookControllerTest.java
+++ src/test/java/com/example/demotest/book/BookControllerTest.java
@@ -2,22 +2,50 @@ package com.example.demotest.book;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 // Supermoc [Spring]: uruchom Spring'a przed testem (cały kontekst)
 @SpringBootTest
+// Supermoc - Spring będzie wiedział, jak utworzyć obiekt typu MockMvc
+@AutoConfigureMockMvc
 class BookControllerTest {
+
+    /*
+        W teście wstrzykiwanie przez pole jak najbardziej OK.
+        MockMvc dostarcza metody do testów endpointów Spring MVC
+     */
+    @Autowired
+    private MockMvc mockMvc;
+
     // Supermoc[JUnit]: test
     @Test
     // Supermoc[JUnit]: w liście wyświetlanych testów info z @DisplayName, amiast nazwy testu
-    @DisplayName("GET /books -> HTTP 200, lista książek {JSON}")
-    void whenGetBooks_thenReturn200AndAllBooks() {
+    @DisplayName("GET /books -> HTTP 200")
+    void whenGetBooks_thenReturnHTTP200() throws Exception {
         /*
             Nie zostawiamy pustych testów!
             Wyjątki np. contextLoads();
-         */
         fail();
+         */
+        var endpointURL = "/books";
+        mockMvc
+                // Wyślij request HTTP GET na endpoint /books
+                .perform(
+                        get(endpointURL)
+                )
+                // dodatkowe logowanie
+                .andDo(print())
+                // Oczekuj HTTP 200
+                .andExpect(status().isOk());
     }
 }
